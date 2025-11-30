b2093699f2efab2c43cbd7b698f0cc040b04969a
DeveshRathod <devnaik9302@gmail.com>
2025-12-01 04:58:17 +0530

    added sg module

---
+++ modules/sg/vars.tf
@@ -0,0 +1,28 @@
+variable "sg_name" {
+  description = "Name of the security Group"
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+}
+
+variable "sg_ports" {
+  description = "Ports for security group"
+  type = list(object({
+    from = number
+    to   = number
+    cidr = list(string)
+    description = string
+    protocol = string
+  }))
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the security group will be created"
+}
+
+variable "sg_tags" {
+  description = "Tags to be applied to the security group"
+  type        = map(string)
+  default     = {}
+}
