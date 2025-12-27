7944f8c7e62161b1ab5254dec8901484ae51fcb8
DeveshRathod <devnaik9302@gmail.com>
2025-12-27 19:50:04 +0530

    added qa env

---
+++ environments/qa/main.tf
@@ -0,0 +1,169 @@
+################################################################################################################
+# VPC Module
+################################################################################################################
+module "vpc_module" {
+  source = "../../modules/vpc"
+
+  vpc_name     = var.vpc_name
+  cidr_for_vpc = var.vpc_cidr
+  aws_region   = var.aws_region
+
+  private_subnets      = var.private_subnets
+  public_subnets       = var.public_subnets
+  nat_attached_subnets = var.nat_attached_subnets
+
+  nat_attached = var.nat_attached
+  ig_attached  = var.ig_attached
+
+  private_subnets_cidr = var.private_subnets_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  nat_subnets_cidr     = var.nat_subnets_cidr
+}
+
+################################################################################################################
+# SG Module
+################################################################################################################
+module "sg_module" {
+  source = "../../modules/sg"
+
+  sg_name    = var.sg_name
+  sg_description = var.sg_description
+  vpc_id     = module.vpc_module.vpc_id
+  aws_region = var.aws_region
+  sg_ports   = var.sg_ports
+}
+
+################################################################################################################
+# SM Module
+################################################################################################################
+module "sm_module" {
+  source = "../../modules/sm"
+
+  aws_region         = var.aws_region
+  secret_description = var.secret_description
+  secret_for_sm      = var.secret_for_sm
+  secret_name        = var.secret_name
+  secret_tags        = var.secret_tags
+}
+
+################################################################################################################
+# IAM Module
+################################################################################################################
+module "iam_module" {
+  source                    = "../../modules/iam"
+  aws_region                = var.aws_region
+  custom_policy_description = var.custom_policy_description
+  custom_policy_name        = var.custom_policy_name
+  execution_policy_arn      = var.execution_policy_arn
+  role_name                 = var.role_name
+  s3_bucket_name            = var.s3_bucket_name
+  tags                      = var.tags
+}
+
+################################################################################################################
+# NS Module
+################################################################################################################
+module "ns_module" {
+  source = "../../modules/ns"
+
+  aws_region            = var.aws_region
+  namespace_description = var.namespace_description
+  namespace_tags        = var.namespace_tags
+  namespace_name        = var.namespace_name
+  vpc_id                = module.vpc_module.vpc_id
+}
+
+################################################################################################################
+# Endpoints Module
+################################################################################################################
+module "endpoints_module" {
+  source = "../../modules/endpoints"
+
+  vpc_id               = module.vpc_module.vpc_id
+  aws_region           = var.aws_region
+  private_subnets      = module.vpc_module.nat_private_subnets_ids
+  private_route_tables = module.vpc_module.all_private_route_table_ids
+  security_group_id    = module.sg_module.sg_id
+
+  enable_logs           = var.enable_logs
+  enable_ecr_api        = var.enable_ecr_api
+  enable_ecr_dkr        = var.enable_ecr_dkr
+  enable_s3_gateway     = var.enable_s3_gateway
+  enable_secretsmanager = var.enable_secretsmanager
+
+  endpoint_tags = var.endpoint_tags
+}
+
+################################################################################################################
+# ECS Module
+################################################################################################################
+module "ecs_module" {
+  source = "../../modules/ecs"
+
+  aws_region = var.aws_region
+
+  cluster_name   = var.cluster_name
+  log_group_name = var.log_group_name
+  log_retention  = var.log_retention
+
+  ecs_family_name    = var.ecs_family_name
+  ecs_image          = var.ecs_image
+  ecs_node_env       = var.ecs_node_env
+  ecs_container_port = var.ecs_container_port
+
+  cpu    = var.cpu
+  memory = var.memory
+
+  network_mode             = var.network_mode
+  requires_compatibilities = var.requires_compatibilities
+  operating_system_family  = var.operating_system_family
+  cpu_architecture         = var.cpu_architecture
+
+  execution_role_arn = module.iam_module.ecs_task_role_arn
+  task_role_arn      = module.iam_module.ecs_task_role_arn
+
+  secret_arn  = module.sm_module.secret_arn
+  secret_keys = var.secret_keys
+
+  healthcheck_command      = var.healthcheck_command
+  healthcheck_interval     = var.healthcheck_interval
+  healthcheck_timeout      = var.healthcheck_timeout
+  healthcheck_retries      = var.healthcheck_retries
+  healthcheck_start_period = var.healthcheck_start_period
+}
+
+################################################################################################################
+# Service Module
+################################################################################################################
+module "service_module" {
+  source = "../../modules/service"
+
+  aws_region = var.aws_region
+
+  service_name        = var.service_name
+  cluster_arn         = module.ecs_module.ecs_cluster_arn
+  cluster_name        = module.ecs_module.ecs_cluster_name
+  task_definition_arn = module.ecs_module.ecs_task_definition_arn
+
+  launch_type      = var.launch_type
+  desired_count    = var.desired_count
+  subnets          = module.vpc_module.nat_private_subnets_ids
+  security_groups  = [module.sg_module.sg_id]
+  assign_public_ip = var.assign_public_ip
+
+  namespace_id    = module.ns_module.aws_service_discovery_private_dns_namespace_id
+  dns_record_type = var.dns_record_type
+  dns_ttl         = var.dns_ttl
+  routing_policy  = var.routing_policy
+
+  min_capacity = var.min_capacity
+  max_capacity = var.max_capacity
+
+  scalable_dimension = var.scalable_dimension
+  service_namespace  = var.service_namespace
+
+  metric_type        = var.metric_type
+  target_value       = var.target_value
+  scale_in_cooldown  = var.scale_in_cooldown
+  scale_out_cooldown = var.scale_out_cooldown
+}
---
+++ environments/qa/variables.tf
@@ -0,0 +1,366 @@
+################################################################################################################
+# GLOBAL VARIABLES
+################################################################################################################
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+################################################################################################################
+# VPC VARIABLES
+################################################################################################################
+variable "vpc_name" {
+  description = "Name for VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR range for VPC"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "Number of private subnets to create"
+  type        = number
+}
+
+variable "public_subnets" {
+  description = "Number of public subnets to create"
+  type        = number
+}
+
+variable "nat_attached_subnets" {
+  description = "Number of subnets for NAT gateways"
+  type        = number
+}
+
+variable "nat_attached" {
+  description = "Whether to attach NAT gateway"
+  type        = bool
+}
+
+variable "sg_description" {
+  description = "Description for sg"
+}
+
+variable "ig_attached" {
+  description = "Whether to attach Internet Gateway"
+  type        = bool
+}
+
+variable "private_subnets_cidr" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnets_cidr" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+}
+
+variable "nat_subnets_cidr" {
+  description = "List of CIDRs for NAT subnets"
+  type        = list(string)
+}
+
+################################################################################################################
+# SG VARIABLES
+################################################################################################################
+variable "sg_name" {
+  description = "Security group name"
+  type        = string
+}
+
+variable "sg_ports" {
+  description = "List of SG port rules"
+  type = list(object({
+    from        = number
+    to          = number
+    protocol    = string
+    cidr        = list(string)
+    description = string
+  }))
+}
+
+################################################################################################################
+# NS VARIABLES
+################################################################################################################
+variable "namespace_name" {
+  type        = string
+  description = "Private DNS Namespace name"
+}
+
+variable "namespace_description" {
+  type        = string
+  description = "Description for the namespace"
+}
+
+variable "namespace_tags" {
+  type        = map(string)
+  description = "Tags for the namespace"
+}
+
+################################################################################################################
+# SM VARIABLES
+################################################################################################################
+variable "secret_name" {
+  type        = string
+  description = "Name of the secret in Secrets Manager"
+}
+
+variable "secret_description" {
+  type        = string
+  description = "Description of the secret"
+}
+
+variable "secret_tags" {
+  type        = map(string)
+  description = "Tags to apply to the secret"
+}
+
+variable "secret_for_sm" {
+  type        = map(any)
+  description = "The secret data in JSON format"
+}
+
+################################################################################################################
+# IAM VARIABLES
+################################################################################################################
+variable "role_name" {
+  type        = string
+  description = "Name of the ECS task IAM role"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to assign to the ECS IAM role"
+}
+
+variable "custom_policy_name" {
+  type        = string
+  description = "Name of the custom IAM policy to create"
+}
+
+variable "execution_policy_arn" {
+  type        = string
+  description = "ARN of standard ECS execution policy to attach"
+}
+
+variable "custom_policy_description" {
+  type        = string
+  description = "Description of the custom IAM policy"
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "S3 bucket name to allow access for ECS tasks"
+}
+
+################################################################################################################
+# ECS VARIABLES (TASK DEFINITION)
+################################################################################################################
+variable "ecs_family_name" {
+  type        = string
+  description = "Name of the ECS task definition family (also used as the container name)."
+}
+
+variable "ecs_image" {
+  type        = string
+  description = "Full Docker image URI (ECR or public registry) used by the ECS container."
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the ECS cluster where the service and tasks will run."
+}
+
+variable "log_group_name" {
+  type        = string
+  description = "CloudWatch Log Group name for storing application logs."
+}
+
+variable "log_retention" {
+  type        = number
+  description = "Retention period (in days) for CloudWatch logs."
+}
+
+variable "cpu" {
+  type        = string
+  description = "Amount of CPU units allocated to the ECS task definition."
+}
+
+variable "memory" {
+  type        = string
+  description = "Amount of memory (in MiB) allocated to the ECS task definition."
+}
+
+variable "network_mode" {
+  type        = string
+  description = "Docker networking mode for the ECS task (e.g., awsvpc, bridge)."
+}
+
+variable "requires_compatibilities" {
+  type        = list(string)
+  description = "Launch type for ECS (e.g., [\"FARGATE\"], [\"EC2\"])."
+}
+
+variable "operating_system_family" {
+  type        = string
+  description = "Operating system for the ECS task definition (e.g., LINUX)."
+}
+
+variable "cpu_architecture" {
+  type        = string
+  description = "CPU architecture for the container (e.g., X86_64, ARM64)."
+}
+
+variable "ecs_container_port" {
+  type        = number
+  description = "Port on which the container listens for incoming traffic."
+}
+
+variable "ecs_node_env" {
+  type        = string
+  description = "Application runtime environment (dev/stage/prod)."
+}
+
+variable "secret_keys" {
+  type        = list(string)
+  description = "List of secret environment variable names mapped from AWS Secrets Manager."
+}
+
+variable "healthcheck_command" {
+  type        = list(string)
+  description = "Command executed by ECS to determine container health."
+}
+
+variable "healthcheck_interval" {
+  type        = number
+  description = "Time (in seconds) between health check executions."
+}
+
+variable "healthcheck_timeout" {
+  type        = number
+  description = "Time (in seconds) after which a health check is considered failed."
+}
+
+variable "healthcheck_retries" {
+  type        = number
+  description = "Number of retry attempts before marking the container unhealthy."
+}
+
+variable "healthcheck_start_period" {
+  type        = number
+  description = "Grace period (in seconds) before starting health checks."
+}
+
+################################################################################################################
+# ECS SERVICE VARIABLES 
+################################################################################################################
+variable "service_name" {
+  type        = string
+  description = "Name of the ECS service to be created."
+}
+
+variable "launch_type" {
+  type        = string
+  description = "Launch type for ECS service (FARGATE or EC2)."
+}
+
+variable "desired_count" {
+  type        = number
+  description = "Number of desired tasks for the ECS service."
+}
+
+variable "assign_public_ip" {
+  type        = bool
+  description = "Assigns public IP to tasks (only for awsvpc mode)."
+}
+
+variable "dns_record_type" {
+  type        = string
+  description = "DNS record type for service discovery (A or SRV)."
+}
+
+variable "dns_ttl" {
+  type        = number
+  description = "TTL for Cloud Map DNS records."
+}
+
+variable "routing_policy" {
+  type        = string
+  description = "Routing policy for Cloud Map (MULTIVALUE or WEIGHTED)."
+}
+
+variable "min_capacity" {
+  type        = number
+  description = "Minimum tasks for ECS auto scaling."
+}
+
+variable "max_capacity" {
+  type        = number
+  description = "Maximum tasks for ECS auto scaling."
+}
+
+variable "metric_type" {
+  type        = string
+  description = "CloudWatch metric for auto scaling (CPU or Memory)."
+}
+
+variable "target_value" {
+  type        = number
+  description = "Target value for auto scaling metric."
+}
+
+variable "scale_in_cooldown" {
+  type        = number
+  description = "Scale-in cooldown period in seconds."
+}
+
+variable "scale_out_cooldown" {
+  type        = number
+  description = "Scale-out cooldown period in seconds."
+}
+
+variable "service_namespace" {
+  type        = string
+  description = "Auto Scaling namespace (ecs)."
+}
+
+variable "scalable_dimension" {
+  type        = string
+  description = "Scalable dimension (ecs:service:DesiredCount)."
+}
+
+################################################################################################################
+# ENDPOINT VARIABLES
+################################################################################################################
+variable "enable_logs" {
+  type        = bool
+  description = "Enable VPC endpoint for CloudWatch Logs."
+}
+
+variable "enable_ecr_api" {
+  type        = bool
+  description = "Enable VPC endpoint for ECR API."
+}
+
+variable "enable_ecr_dkr" {
+  type        = bool
+  description = "Enable VPC endpoint for ECR Docker Registry."
+}
+
+variable "enable_s3_gateway" {
+  type        = bool
+  description = "Enable S3 Gateway Endpoint for private S3 access."
+}
+
+variable "enable_secretsmanager" {
+  type        = bool
+  description = "Enable VPC endpoint for Secrets Manager."
+}
+
+variable "endpoint_tags" {
+  type        = map(string)
+  description = "Tags to apply to all VPC endpoints."
+}
